01428f613aea4b7665af07c240aa79a8a222a41f
Lai Weng Han <jlwhoo7@gmail.com>
2018-02-20 08:15:34 +0800

    Added channel restriction

--- src/index.js
+++ src/index.js
@@ -45,217 +45,226 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    // GET INFO FOR MESSAGE
-    let {
-        id: currentMessageId,
-        author: {
-            username: currentUsername,
-            id: currentUserId
-        },
-        content: currentContent,
-        createdTimestamp: currentCreatedTimestamp
-    } = msg;
-
-    if (currentUserId === config.botId) {
-        logger.info('BOT MESSAGE:', currentContent);
-    } else if (
-        config.whitelistId.indexOf(currentUserId) !== -1
-    ) {
-        logger.info('ADMIN MESSAGE', currentContent);
+    if (msg.channel.id !== config.channelId) {
+        return;
     } else {
-        let currentCreatedTime = getDateTimeFromTimestamp(
-            currentCreatedTimestamp
-        );
+        // GET INFO FOR MESSAGE
+        let {
+            id: currentMessageId,
+            author: {
+                username: currentUsername,
+                id: currentUserId
+            },
+            content: currentContent,
+            createdTimestamp: currentCreatedTimestamp
+        } = msg;
 
-        logger.info(
-            `${currentUsername} (${currentUserId}) created "${currentContent}" on ${currentCreatedTime} (msg_id: ${currentMessageId})`
-        );
-        logger.info(currentContent);
-
-        let result = new Promise((resolve, reject) => {
-            // Check Registered User
-            checkRegisteredUser(currentUserId).then(
-                isRegistered => {
-                    console.log(isRegistered);
-                    if (isRegistered) {
-                        resolve('');
-                    } else {
-                        reject('NOT_REGISTERED');
-                    }
-                }
+        if (currentUserId === config.botId) {
+            logger.info('BOT MESSAGE:', currentContent);
+        } else if (
+            config.whitelistId.indexOf(currentUserId) !== -1
+        ) {
+            logger.info('ADMIN MESSAGE', currentContent);
+        } else {
+            let currentCreatedTime = getDateTimeFromTimestamp(
+                currentCreatedTimestamp
             );
-        })
-            .then(() => {
-                // Check Post Validity with Regex (/(http|https):\/\/(www\.steemit\.com\/|steemit\.com\/|busy\.org\/|www\.busy\.org\/)/g)
-                let isPostValid = !!currentContent.match(
-                    /(http|https):\/\/(www\.steemit\.com\/|steemit\.com\/)/g
-                );
-                if (isPostValid) {
-                    return;
-                } else {
-                    throw 'NOT_VALID_LINK';
-                }
-            })
-            .then(() => {
-                // Check Last Post Time
-                return checkLastPost(currentUserId)
-                    .then(data => {
-                        if (!!data) {
-                            timeDiff = Math.floor(
-                                (currentCreatedTimestamp -
-                                    data) /
-                                    1000
-                            );
-                            console.log(timeDiff);
-                            // CHECK TIME
-                            if (
-                                timeDiff >
-                                config.timeAllowed
-                            ) {
-                                // Proceed
-                            } else {
-                                throw 'NOT_YET_TIME';
-                                return;
-                            }
+
+            logger.info(
+                `${currentUsername} (${currentUserId}) created "${currentContent}" on ${currentCreatedTime} (msg_id: ${currentMessageId})`
+            );
+            logger.info(currentContent);
+
+            let result = new Promise((resolve, reject) => {
+                // Check Registered User
+                checkRegisteredUser(currentUserId).then(
+                    isRegistered => {
+                        console.log(isRegistered);
+                        if (isRegistered) {
+                            resolve('');
                         } else {
-                            // First Time
-                            msg.reply(
-                                `seems like it is the first time you post here.`
-                            );
+                            reject('NOT_REGISTERED');
                         }
+                    }
+                );
+            })
+                .then(() => {
+                    // Check Post Validity with Regex (/(http|https):\/\/(www\.steemit\.com\/|steemit\.com\/|busy\.org\/|www\.busy\.org\/)/g)
+                    let isPostValid = !!currentContent.match(
+                        /(http|https):\/\/(www\.steemit\.com\/|steemit\.com\/)/g
+                    );
+                    if (isPostValid) {
+                        return;
+                    } else {
+                        throw 'NOT_VALID_LINK';
+                    }
+                })
+                .then(() => {
+                    // Check Last Post Time
+                    return checkLastPost(currentUserId)
+                        .then(data => {
+                            if (!!data) {
+                                timeDiff = Math.floor(
+                                    (currentCreatedTimestamp -
+                                        data) /
+                                        1000
+                                );
+                                console.log(timeDiff);
+                                // CHECK TIME
+                                if (
+                                    timeDiff >
+                                    config.timeAllowed
+                                ) {
+                                    // Proceed
+                                } else {
+                                    throw 'NOT_YET_TIME';
+                                    return;
+                                }
+                            } else {
+                                // First Time
+                                msg.reply(
+                                    `seems like it is the first time you post here.`
+                                );
+                            }
 
-                        // Quality Check
-                        // ============================================================
-                        // Parse link (asd https://steemit.com/ asd => https://steemit.com)
-                        let link = currentContent.match(
-                            /(https?:\/\/[^\s]+)/g
-                        );
-
-                        if (link.length === 1) {
-                            let authorName = link[0].split(
-                                /[\/#]/
-                            )[4];
-                            let permlinkName = link[0].split(
-                                /[\/#]/
-                            )[5];
+                            // Quality Check
                             // ============================================================
-                            // parse out author and permlink and check wether is correct
-                            if (
-                                authorName.charAt(0) ===
-                                    '@' &&
-                                !!permlinkName
-                            ) {
-                                return main(
-                                    authorName.substr(1),
-                                    permlinkName,
-                                    postConfig
-                                )
-                                    .then(postData => {
-                                        if (
-                                            postData.msg ===
-                                                'CHEETAH' ||
-                                            postData.msg ===
-                                                'OLD_POST' ||
-                                            postData.msg ==
-                                                'POST_NOT_FOUND'
-                                        ) {
-                                            throw postData.msg;
-                                        }
-                                        console.log(
-                                            postData
-                                        );
-                                        return postData;
-                                    })
-                                    .catch(err => {
-                                        throw err;
-                                    });
+                            // Parse link (asd https://steemit.com/ asd => https://steemit.com)
+                            let link = currentContent.match(
+                                /(https?:\/\/[^\s]+)/g
+                            );
+
+                            if (link.length === 1) {
+                                let authorName = link[0].split(
+                                    /[\/#]/
+                                )[4];
+                                let permlinkName = link[0].split(
+                                    /[\/#]/
+                                )[5];
+                                // ============================================================
+                                // parse out author and permlink and check wether is correct
+                                if (
+                                    authorName.charAt(0) ===
+                                        '@' &&
+                                    !!permlinkName
+                                ) {
+                                    return main(
+                                        authorName.substr(
+                                            1
+                                        ),
+                                        permlinkName,
+                                        postConfig
+                                    )
+                                        .then(postData => {
+                                            if (
+                                                postData.msg ===
+                                                    'CHEETAH' ||
+                                                postData.msg ===
+                                                    'OLD_POST' ||
+                                                postData.msg ==
+                                                    'POST_NOT_FOUND'
+                                            ) {
+                                                throw postData.msg;
+                                            }
+                                            console.log(
+                                                postData
+                                            );
+                                            return postData;
+                                        })
+                                        .catch(err => {
+                                            throw err;
+                                        });
+                                } else {
+                                    return 'NOT_VALID_LINK';
+                                }
                             } else {
                                 return 'NOT_VALID_LINK';
                             }
-                        } else {
-                            return 'NOT_VALID_LINK';
-                        }
-                    })
-                    .then(postData => {
-                        return upvote(
-                            process.env.STEEM_POSTING,
-                            process.env.STEEM_USERNAME,
-                            postData.author,
-                            postData.permlink,
-                            postData.weightage
-                        )
-                            .then(upvoteData => {
-                                if (
-                                    upvoteData === 'ERROR'
-                                ) {
-                                    throw 'NO_UPVOTE';
-                                } else {
-                                    msg.reply(postData.msg);
-                                    Promise.resolve();
-                                }
-                            })
-                            .catch(err => {
-                                throw err;
-                            });
-                    })
-                    .then(() => {
-                        // UPDATE TIME QUERY
-                        return updateTime(
-                            currentUserId,
-                            currentCreatedTimestamp
-                        );
-                    })
-                    .catch(err => {
-                        throw err;
-                    });
-            })
-            .catch(err => {
-                console.log(err);
-                msg.delete();
-                switch (err) {
-                    case 'NOT_REGISTERED':
-                        msg.reply(
-                            'You are not yet registered'
-                        );
-                        break;
-                    case 'NOT_VALID_LINK':
-                        msg.reply(
-                            'You did not include link to steemit.com'
-                        );
-                        break;
-                    case 'NOT_YET_TIME':
-                        msg.reply(
-                            `Please wait for ${timeConvertMessage(
-                                convert(
-                                    config.timeAllowed -
-                                        timeDiff
-                                )
-                            )}`
-                        );
-                        break;
-                    case 'CHEETAH':
-                        msg.reply(
-                            'Your post is voted by @cheetah'
-                        );
-                        break;
-                    case 'OLD_POST':
-                        msg.reply(
-                            'Your post is too new / old to be shared'
-                        );
-                        break;
-                    case 'POST_NOT_FOUND':
-                        msg.reply('Post not found');
-                        break;
-                    case 'NO_UPVOTE':
-                        msg.reply(
-                            "Can't upvote, it could be upvoted already"
-                        );
-                        break;
-                    default:
-                        msg.reply('ERROR');
-                        break;
-                }
-            });
+                        })
+                        .then(postData => {
+                            return upvote(
+                                process.env.STEEM_POSTING,
+                                process.env.STEEM_USERNAME,
+                                postData.author,
+                                postData.permlink,
+                                postData.weightage
+                            )
+                                .then(upvoteData => {
+                                    if (
+                                        upvoteData ===
+                                        'ERROR'
+                                    ) {
+                                        throw 'NO_UPVOTE';
+                                    } else {
+                                        msg.reply(
+                                            postData.msg
+                                        );
+                                        Promise.resolve();
+                                    }
+                                })
+                                .catch(err => {
+                                    throw err;
+                                });
+                        })
+                        .then(() => {
+                            // UPDATE TIME QUERY
+                            return updateTime(
+                                currentUserId,
+                                currentCreatedTimestamp
+                            );
+                        })
+                        .catch(err => {
+                            throw err;
+                        });
+                })
+                .catch(err => {
+                    console.log(err);
+                    msg.delete();
+                    switch (err) {
+                        case 'NOT_REGISTERED':
+                            msg.reply(
+                                'You are not yet registered'
+                            );
+                            break;
+                        case 'NOT_VALID_LINK':
+                            msg.reply(
+                                'You did not include link to steemit.com'
+                            );
+                            break;
+                        case 'NOT_YET_TIME':
+                            msg.reply(
+                                `Please wait for ${timeConvertMessage(
+                                    convert(
+                                        config.timeAllowed -
+                                            timeDiff
+                                    )
+                                )}`
+                            );
+                            break;
+                        case 'CHEETAH':
+                            msg.reply(
+                                'Your post is voted by @cheetah'
+                            );
+                            break;
+                        case 'OLD_POST':
+                            msg.reply(
+                                'Your post is too new / old to be shared'
+                            );
+                            break;
+                        case 'POST_NOT_FOUND':
+                            msg.reply('Post not found');
+                            break;
+                        case 'NO_UPVOTE':
+                            msg.reply(
+                                "Can't upvote, it could be upvoted already"
+                            );
+                            break;
+                        default:
+                            msg.reply('ERROR');
+                            break;
+                    }
+                });
+        }
     }
 });
 client.login(process.env.DISCORD_TOKEN); // Start server
